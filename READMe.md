52dbb8c593db94890e13b4a9a57f7b072e9e785e
vxggangxv <vxggangxv@naver.com>
2019-04-18 15:53:18 +0900

    w3-sass-template

---
+++ READMe.md
@@ -0,0 +1,10 @@
+# VSCODE Template
+
+- include-html (w3_inc.js)
+- scss (live-sass)
+
+- ruby 설치 후 
+```bash
+$ gen install sass
+$ sass --watch public/sass:public/css --style compact
+```
---
+++ public/js/w3_inc.js
@@ -0,0 +1,30 @@
+$(function() {
+    // include 적용
+    w3.includeHTML();
+});
+
+var w3 = {};
+
+w3.includeHTML = function(cb) {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      file = elmnt.getAttribute("include-html");
+      if (file) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            elmnt.removeAttribute("include-html");
+            w3.includeHTML(cb);
+          }
+        }      
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        return;
+      }
+    }
+    if (cb) cb();
+  };
